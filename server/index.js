98f988c4bf28db9b7e2cb8baa026f6d482115ae5
Juan Gabriel Espinoza <50055314+espinozajuan@users.noreply.github.com>
2022-06-08 14:19:36 -0300

    remove heroku

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ import productRoutes from './routes/productRoutes.js';
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, './client/build')));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
