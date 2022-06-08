c0b59c1e35d46032fc299decdb10c6170e1da55e
Juan Gabriel Espinoza <50055314+espinozajuan@users.noreply.github.com>
2022-06-08 12:29:11 -0300

    heroku server

--- server/index.js
+++ server/index.js
@@ -1,20 +1,21 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import express from 'express'
-import cors from 'cors'
-import errorHandler from './middleware/errorMidddleware.js'
-const port = process.env.PORT || 5000
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+import errorHandler from './middleware/errorMidddleware.js';
+const port = process.env.PORT || 5000;
 
-import productRoutes from './routes/productRoutes.js' 
+import productRoutes from './routes/productRoutes.js';
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/items', productRoutes)
+app.use('/api/items', productRoutes);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server running on port: ${port}`))
+app.listen(port, () => console.log(`Server running on port: ${port}`));
